78d11fc8218641811bde7ac858713e6224a72bd4
putao520 <yuyao1022@hotmail.com>
2023-03-14 01:38:07 +0800

    11

---
+++ src/assets/svg/down-across.js
@@ -0,0 +1,5 @@
+export default function DownAcrossSvg() {
+    return <svg version="1.1" role="presentation" width="10" height="16" viewBox="0 0 12 12" display="inline-block" fill="currentColor" xmlns="http://www.w3.org/2000/svg">  <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0">
+        <path d="M10.375,3.219,6,6.719l-4.375-3.5A1,1,0,1,0,.375,4.781l5,4a1,1,0,0,0,1.25,0l5-4a1,1,0,0,0-1.25-1.562Z"></path>
+    </g></svg>
+}
---
+++ src/assets/svg/window.js
@@ -0,0 +1,18 @@
+export default function WindowSvg() {
+    return <svg version="1.1" role="presentation" width="16" height="16" viewBox="0 0 24 24" className="mo-icon MoIcon">
+        <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0">
+            <path
+                d="M9.436 2.331l-9 1.174A.5.5 0 0 0 0 4v7a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V2.827a.497.497 0 0 0-.17-.375.491.491 0 0 0-.394-.121z">
+            </path>
+            <path
+                d="M23.83.625a.486.486 0 0 0-.395-.12l-12 1.565a.5.5 0 0 0-.435.496V11a.5.5 0 0 0 .5.5h12a.5.5 0 0 0 .5-.5V1a.499.499 0 0 0-.17-.375z">
+            </path>
+            <path
+                d="M9.5 12.5h-9a.5.5 0 0 0-.5.5v7c0 .251.187.463.436.496l9 1.174a.499.499 0 0 0 .564-.496V13a.5.5 0 0 0-.5-.5z">
+            </path>
+            <path
+                d="M23.5 12.5h-12a.5.5 0 0 0-.5.5v8.435c0 .251.187.463.436.496l12 1.565A.499.499 0 0 0 24 23V13a.5.5 0 0 0-.5-.5z">
+            </path>
+        </g>
+    </svg>
+}
